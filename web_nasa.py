adb41ad653ee8fad3ee648fc98664f6073c1cbda
sokolovisokolovi <sokolovisokolovi@yandex.ru>
2023-08-27 22:11:28 +0300

    4#

--- web_nasa.py
+++ web_nasa.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    get_up = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/Perseverance/latest_photos?api_key=DEMO_KEY")
+    json_data = json.loads(get_up.text)
+    photo = json_data["latest_photos"]
+    return render_template("index.html", photo=photo)
 
 app.run(debug=True)
