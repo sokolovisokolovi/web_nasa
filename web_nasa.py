601f05b2987b71ac87411814330ee1d14a4919d0
sokolovisokolovi <sokolovisokolovi@yandex.ru>
2023-08-27 21:38:45 +0300

    3#

--- web_nasa.py
+++ web_nasa.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app_route("/")
-def mars_foto():
-    return "<p>Hello World!<p>"
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 app.run(debug=True)
